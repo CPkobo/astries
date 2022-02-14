b7bf8977af91d638e63f4d04ead1c842caf1c781
Dianzi Yang <sheepy.workshop@gmail.com>
2022-02-15 06:26:27 +0900

    amend build option, so that the builder can work

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,16 @@
 export default /** @type {import('astro').AstroUserConfig} */ ({
 	renderers: [
 		'@astrojs/renderer-svelte',
-		// "@astrojs/renderer-vue",
-		// "@astrojs/renderer-react",
-		// "@astrojs/renderer-preact"
+		"@astrojs/renderer-vue",
+		"@astrojs/renderer-react",
+		"@astrojs/renderer-preact"
 	],
 	// projectRoot: "./",
 	// public: "public",
 	// dist: "dist",
-	// buildOptions: {
-	// 	sitemap: true,
-	// 	site: "https://example.com",
-		// pageUrlFormat: "file"
-	// }
+	buildOptions: {
+		sitemap: true,
+		site: "https://example.com",
+		pageUrlFormat: "file"
+	}
 });
